9a739261e5f7065b73a8ae87eec9e3db6c450223
alabobriggs <alabobriggs@gmail.com>
2020-08-31 11:44:25 +0100

    add method to add a new place

--- lib/providers/great_places_provider.dart
+++ lib/providers/great_places_provider.dart
@@ -1,5 +1,5 @@
+import 'dart:io';
 import 'package:flutter/foundation.dart';
-
 import '../models/place.dart';
 
 class GreatPlacesProvider with ChangeNotifier {
@@ -7,5 +7,19 @@ class GreatPlacesProvider with ChangeNotifier {
 
   List<Place> get items {
     return [..._items];
+  }
+
+  void addPlace(
+    String pickedTitle,
+    File pickedImage,
+  ) {
+    final newPlace = Place(
+      id: DateTime.now().toString(),
+      image: pickedImage,
+      title: pickedTitle,
+      location: null,
+    );
+    _items.add(newPlace);
+    notifyListeners();
   }
 }
