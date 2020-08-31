7cd712dabc9c68868448691f29a5d854f58d48cd
alabobriggs <alabobriggs@gmail.com>
2020-08-31 13:45:10 +0100

    add db method on places provider

--- lib/providers/great_places_provider.dart
+++ lib/providers/great_places_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/foundation.dart';
+import '../helpers/db_helpers.dart';
 import '../models/place.dart';
 
 class GreatPlacesProvider with ChangeNotifier {
@@ -20,6 +21,31 @@ class GreatPlacesProvider with ChangeNotifier {
       location: null,
     );
     _items.add(newPlace);
+    notifyListeners();
+    DBHelper.insert(
+      'places',
+      {
+        "id": newPlace.id,
+        "title": newPlace.title,
+        "image": newPlace.image.path,
+      },
+    );
+  }
+
+  Future<void> fetchAndSetPlaces() async {
+    final dataList = await DBHelper.getData('places');
+
+    _items = dataList
+        .map(
+          (item) => Place(
+            id: item['id'],
+            title: item['title'],
+            image: File(item['image']),
+            location: null,
+          ),
+        )
+        .toList();
+
     notifyListeners();
   }
 }
